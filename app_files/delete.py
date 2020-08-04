2c0830a6fb06e35010b658c7c0b8af74f8ce4135
Tejaswa Gavankar <swahareddy@gmail.com>
2020-08-04 12:22:34 +0530

    Create delete.py

---
+++ app_files/delete.py
@@ -0,0 +1,24 @@
+from google.cloud import storage
+import os
+print('Credentials from environ: {}'.format(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')))
+
+
+
+def list_bucket_contents():
+    storage_client = storage.Client()
+    buckets_list = storage_client.list_buckets()
+
+    def list_blobs(bucket_name):
+        blobs = storage_client.list_blobs(bucket_name)
+        files_list=[]
+        for blob in blobs:
+            files_list.append(blob.name)
+        return files_list
+
+    bucket_contents={}
+    for bucket in buckets_list:
+        if ".appspot.com" not in bucket.name:
+            bucket_contents[bucket.name]=list_blobs(bucket.name)
+    return bucket_contents
+print(list_bucket_contents())
+
